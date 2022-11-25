4fc80a8209a7d2a6ed8ffc5fce3e7cc3632f1955
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2022-11-26 01:32:11 +0300

    #1: Add ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:2.1.3")
+    implementation("io.ktor:ktor-server-netty-jvm:2.1.3")
+    implementation("io.ktor:ktor-server-status-pages-jvm:2.1.3")
+    implementation("io.ktor:ktor-server-default-headers-jvm:2.1.3")
+
     testImplementation(kotlin("test"))
 }
 
