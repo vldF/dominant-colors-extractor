8b01bd6b55e0e43b44c43773f9e5be1e6c1cb9cf
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2022-11-26 17:20:41 +0300

    #1: Introduce ColorSerializer

---
+++ src/main/kotlin/me/vldf/colors/serializers/ColorSerializer.kt
@@ -0,0 +1,31 @@
+package me.vldf.colors.serializers
+
+import com.github.ajalt.colormath.model.RGB
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.encodeStructure
+
+object ColorSerializer : KSerializer<RGB> {
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Color") {
+        this.element("r", PrimitiveSerialDescriptor("red", PrimitiveKind.INT))
+        this.element("g", PrimitiveSerialDescriptor("green", PrimitiveKind.INT))
+        this.element("b", PrimitiveSerialDescriptor("blue", PrimitiveKind.INT))
+    }
+
+    override fun deserialize(decoder: Decoder): RGB {
+        return RGB.from255(decoder.decodeInt(), decoder.decodeInt(), decoder.decodeInt())
+    }
+
+    override fun serialize(encoder: Encoder, value: RGB) {
+        encoder.encodeStructure(descriptor) {
+            encodeIntElement(descriptor, 0, value.redInt)
+            encodeIntElement(descriptor, 1, value.greenInt)
+            encodeIntElement(descriptor, 2, value.blueInt)
+        }
+    }
+}
