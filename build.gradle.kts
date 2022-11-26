2cfda278ab036754b1d6e0324831a2e122535eda
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2022-11-26 17:13:55 +0300

    #1: Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,30 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
 }
 
 group = "me.vldf"
 version = "1.0-SNAPSHOT"
+
+val ktorVersion = "2.1.3"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-netty-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-status-pages-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-default-headers-jvm:2.1.3")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("org.slf4j:slf4j-api:2.0.4")
+    implementation("org.slf4j:slf4j-simple:2.0.4")
+
+    implementation("com.github.ajalt.colormath:colormath:3.2.1")
+    implementation("com.github.haifengl:smile-core:2.6.0")
 
     testImplementation(kotlin("test"))
 }
