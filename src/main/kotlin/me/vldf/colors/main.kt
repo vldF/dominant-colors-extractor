a706ad233bb61acac3e93499cb3b45f475c5c386
Vladislav Feofilaktov <vladislavf7@gmail.com>
2022-11-28 00:12:47 +0300

    #4: Add color format parameters

--- src/main/kotlin/me/vldf/colors/main.kt
+++ src/main/kotlin/me/vldf/colors/main.kt
@@ -6,27 +6,22 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.contextual
+import kotlinx.serialization.ExperimentalSerializationApi
 import me.vldf.colors.controllers.api.getThemeRoute
-import me.vldf.colors.serializers.ColorSerializer
 
+@OptIn(ExperimentalSerializationApi::class)
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         install(Routing)
         install(ContentNegotiation) {
-            json(Json {
-                this.serializersModule = SerializersModule {
-                    this.contextual(ColorSerializer)
-                }
-            })
+            json()
         }
 
         configureApiRoutes()
     }.start(wait = true)
 }
 
+@ExperimentalSerializationApi
 private fun Application.configureApiRoutes() {
     routing {
         route("api") {
--- src/main/kotlin/me/vldf/colors/serializers/RgbHexColorSerializer.kt
+++ src/main/kotlin/me/vldf/colors/serializers/RgbHexColorSerializer.kt
@@ -1,5 +1,6 @@
 package me.vldf.colors.serializers
 
+import com.github.ajalt.colormath.RenderCondition
 import com.github.ajalt.colormath.model.RGB
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
@@ -10,22 +11,19 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.encoding.encodeStructure
 
-object ColorSerializer : KSerializer<RGB> {
+
+object RgbHexColorSerializer : KSerializer<RGB> {
     override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Color") {
-        this.element("r", PrimitiveSerialDescriptor("red", PrimitiveKind.INT))
-        this.element("g", PrimitiveSerialDescriptor("green", PrimitiveKind.INT))
-        this.element("b", PrimitiveSerialDescriptor("blue", PrimitiveKind.INT))
+        this.element("code", PrimitiveSerialDescriptor("code", PrimitiveKind.STRING))
     }
 
     override fun deserialize(decoder: Decoder): RGB {
-        return RGB.from255(decoder.decodeInt(), decoder.decodeInt(), decoder.decodeInt())
+        return RGB(decoder.decodeString())
     }
 
     override fun serialize(encoder: Encoder, value: RGB) {
         encoder.encodeStructure(descriptor) {
-            encodeIntElement(descriptor, 0, value.redInt)
-            encodeIntElement(descriptor, 1, value.greenInt)
-            encodeIntElement(descriptor, 2, value.blueInt)
+            encodeStringElement(descriptor, 0, value.toHex(renderAlpha = RenderCondition.NEVER))
         }
     }
 }
