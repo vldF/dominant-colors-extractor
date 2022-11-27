ee4da73a9ce9ecc4b49a1d6afe884ceaa519a474
Vladislav Feofilaktov <vladislavf7@gmail.com>
2022-11-27 21:21:38 +0300

    #2: Rename `models` to `services`

--- src/main/kotlin/me/vldf/colors/controllers/api/ThemeController.kt
+++ src/main/kotlin/me/vldf/colors/controllers/api/ThemeController.kt
@@ -6,7 +6,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import me.vldf.colors.model.ImageProcessor
+import me.vldf.colors.services.ImageProcessor
 import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
 
